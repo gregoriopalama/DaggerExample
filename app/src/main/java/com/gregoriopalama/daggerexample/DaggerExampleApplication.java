5ab4181157b9755f20d675224e48036cb3c02b71
Gregorio Palamà <gregoriopalama@gmail.com>
2018-03-30 00:58:15 +0200

    Created custom Application

---
+++ app/src/main/java/com/gregoriopalama/daggerexample/DaggerExampleApplication.java
@@ -0,0 +1,12 @@
+package com.gregoriopalama.daggerexample;
+
+import android.app.Application;
+
+/**
+ * This will be the application. For now, it will be empty
+ *
+ * @author Gregorio Palamà
+ */
+
+public class DaggerExampleApplication extends Application {
+}

e5b62c8c54b34cc2b1b316f2f4bb47d58d136996
Gregorio Palamà <gregoriopalama@gmail.com>
2018-03-30 01:13:10 +0200

    Created AppComponent

---
+++ app/src/main/java/com/gregoriopalama/daggerexample/di/AppComponent.java
@@ -0,0 +1,24 @@
+package com.gregoriopalama.daggerexample.di;
+
+import com.gregoriopalama.daggerexample.DaggerExampleApplication;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjectionModule;
+
+/**
+ * This is the Component for the whole Application. It will generate the dependency tree
+ *
+ * @author Gregorio Palamà
+ */
+
+@Component(modules={AppModule.class, AndroidInjectionModule.class})
+public interface AppComponent {
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        Builder application(DaggerExampleApplication application);
+        AppComponent build();
+    }
+    void inject(DaggerExampleApplication application);
+}

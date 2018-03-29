2fbbd82373002bce71ae806cc30d3022f60f2836
Gregorio Palamà <gregoriopalama@gmail.com>
2018-03-30 01:18:14 +0200

    Injecting the Application

--- app/src/main/java/com/gregoriopalama/daggerexample/DaggerExampleApplication.java
+++ app/src/main/java/com/gregoriopalama/daggerexample/DaggerExampleApplication.java
@@ -1,12 +1,41 @@
 package com.gregoriopalama.daggerexample;
 
+import android.app.Activity;
 import android.app.Application;
 
+import com.gregoriopalama.daggerexample.di.DaggerAppComponent;
+
+import javax.inject.Inject;
+
+import dagger.android.AndroidInjector;
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.HasActivityInjector;
+
 /**
- * This will be the application. For now, it will be empty
+ * This is the application. It uses the generated DaggerAppComponent, and inject itself.
+ * It also implements HasActivityInjector, in order to provide the possibility to inject
+ * activities directly from inside of them.
  *
  * @author Gregorio Palamà
  */
 
-public class DaggerExampleApplication extends Application {
+public class DaggerExampleApplication extends Application implements HasActivityInjector {
+    @Inject
+    DispatchingAndroidInjector<Activity> dispatchingAndroidInjector;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        DaggerAppComponent
+                .builder()
+                .application(this)
+                .build()
+                .inject(this);
+    }
+
+    @Override
+    public AndroidInjector<Activity> activityInjector() {
+        return dispatchingAndroidInjector;
+    }
 }

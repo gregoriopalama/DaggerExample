d8d9d4c4037d79ad7daa6eb8c62c6eb4c694a7b7
Gregorio Palamà <gregoriopalama@gmail.com>
2018-03-30 01:20:06 +0200

    Injecting the Activity

--- app/src/main/java/com/gregoriopalama/daggerexample/ui/MainActivity.java
+++ app/src/main/java/com/gregoriopalama/daggerexample/ui/MainActivity.java
@@ -7,18 +7,26 @@ import android.os.Bundle;
 import com.gregoriopalama.daggerexample.R;
 import com.gregoriopalama.daggerexample.databinding.ActivityMainBinding;
 
+import javax.inject.Inject;
+
+import dagger.android.AndroidInjection;
+
 /**
- * Simple activity. It will show an Hello world Text View.
- * The main goal is to provide an alternative text using Dagger
+ * Simple activity. It now shows Hello Dagger, provided by Dagger
  *
  * @author Gregorio Palamà
  */
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
 
+    @Inject
+    String helloDagger;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AndroidInjection.inject(this);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.greetings.setText(helloDagger);
     }
 }
